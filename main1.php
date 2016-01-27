48562cd42ebf737375a2140424fc4137367b5950
Tony <choonghansheng@hotmail.com>
2016-01-27 16:49:00 +0800

    updated main page with navigation bar

---
+++ main1.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+<link rel="stylesheet" href="main1.css" type="text/css"/>
+<title>Test Navigation</title>
+</head>
+<h1>Welcome to Electronic Medical Record</h1>
+
+<body>
+  <div id ="header">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li>
+            <a href="#">Patient Record &#9662;</a>
+            <ul class="dropdown">
+                <li><a href="#">Add Record</a></li>
+                <li><a href="#">View Record</a></li>
+                <li><a href="#">Update Record</a></li>
+                <li><a href="#">Delete Record</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Patient Profile &#9662;</a>
+            <ul class="dropdown">
+                <li><a href="#">Add Profile</a></li>
+                <li><a href="#">View Profile</a></li>
+                <li><a href="#">Update Profile</a></li>
+                <li><a href="#">Delete Profile</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Insurance Profile &#9662;</a>
+            <ul class="dropdown">
+                <li><a href="#">View Insurance Profile</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Human Resource System &#9662;</a>
+            <ul class="dropdown">
+                <li><a href="#">Add User</a></li>
+                <li><a href="#">View User</a></li>
+                <li><a href="#">Update User</a></li>
+                <li><a href="#">Delete User</a></li>
+            </ul>
+        </li>
+    </ul>
+  </div>
+
+
+
+    <p id = "historyid">Hospital Sultanah Aminah is currently the largest government hospital in Johor Bahru.
+      Hospital Sultanah Aminah had third class wards constructed from wood since 1882.
+       When these wards became old and overcrowded with patients, a new building was planned to replace these old wards.
+        The new building construction began in 1938 and completed in 1941.
+        his 5 storey building can accomodate 857 beds. The building has two wings in the shape of "T" containing 80 beds in each ward.
+        In the center, there are lifts and kitchen for patients. Operation theater is located in first floor and the top floor was specially for TB patients.
+      Accident and Emergency Department used to be located at ground floor of the main building but in 1974 it was shifted to an old building located 300 metres from the main building.
+      Outpatient Department was also located at this building. This building is also not sufficient to provide comfort to the patient.
+       Thus, as an interim solution to improve services in this section, an accident and emergency department was built on the ground floor of the new Operation Theatre complex.
+        It was first used in 27th. May, 1985. This place will continue to be used until the proposed Polyclinic project in Jalan Dato' Wilson which will have a new accident and emergency department is fully implemented.</p>
+
+</body>
+</html>
